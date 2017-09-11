588b3827b898dbf5dcd95124f02aa7dcd38360af
new_user_name <new@email.com>
2017-09-12 02:11:05 +0600

    Initial Commit

---
+++ Home Assignment for Prezi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Home Assignment for Prezi
+//
+//  Created by Admin on 9/12/17.
+//  Copyright © 2017 AAkash. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
